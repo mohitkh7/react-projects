58f3b5484e0616fb83017efd686b95132b12ca16
Mohit Khandelwal <mohitkh7@gmail.com>
2024-01-01 17:51:58 +0530

    completes project 2

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,52 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      setLoading(false);
+      console.log(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  const removeTour = (tour_id) => {
+    const newTours = tours.filter((tour) => tour.id !== tour_id);
+    setTours(newTours);
+  }
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  
+  if (loading) {
+    return <main><Loading /></main>
+  }
+
+  if (tours.length === 0) {
+    return <main>
+    <div className='title'>
+      <h2>No Tours Left</h2>
+      <button className='btn' onClick={() => fetchTours()}>Refresh</button>
+    </div>
+  </main>
+  }
+
+  return <main>
+    <div className='title'>
+      <h2>Our Tours</h2>
+      <div className='underline'></div>
+    </div>
+    <Tours tours={tours} removeTour={removeTour}></Tours>
+  </main>
 }
 
 export default App
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours, removeTour}) => {
+  return <>
+    {tours.map((tour) => <Tour tour={tour} key={tour.id} removeTour={removeTour}></Tour>)}
+  </>
 };
 
 export default Tours;

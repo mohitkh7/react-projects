5b9610f099ca6c5d75a3427c5d2e02f244cd743a
Mohit Khandelwal <mohitkh7@gmail.com>
2024-01-01 19:05:31 +0530

    completed project 3

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,44 @@
 import React, { useState } from 'react';
 import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [reviews, setReviews] = useState(people);
+  const [index, setIndex] = useState(0);
+
+  const changeReview = (index) => {
+    if (index < 0) {
+      index = reviews.length - 1;
+    }
+    else if (index >= reviews.length) {
+      index = 0;
+    }
+    setIndex(index);
+  }
+
+  const randomReview = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    changeReview(randomNumber);
+  };
+
+  return <div className='container'>
+    <div className='review'>
+      <div className='img-container'>
+        <img src={reviews[index].image} className='person-img'/>
+      </div>
+      <h2 className='author'>{reviews[index].name}</h2>
+      <h4 className='job'>{reviews[index].job}</h4>
+      <p className='info'>{reviews[index].text}</p>
+      <div>
+        <button className='prev-btn' onClick={() => changeReview(index-1)}><FaChevronLeft /></button>
+        <button className='random-btn' onClick={() => randomReview()}>Surprise Me</button>
+        <button className='next-btn' onClick={()=>changeReview(index+1)}><FaChevronRight /></button>
+      </div>
+    </div>
+  </div>
 };
 
 export default Review;

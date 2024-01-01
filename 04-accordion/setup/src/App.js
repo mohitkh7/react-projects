98ce936cb237910eb963b0475503660854676227
Mohit Khandelwal <mohitkh7@gmail.com>
2024-01-01 19:35:56 +0530

    completes project 4

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return <main>
+    <section className='container'>
+      <h3>Questions and Answers about login</h3>
+      <div>
+        {questions.map((question) => <SingleQuestion question={question} key={question.id}/>)}
+      </div>
+    </section>
+  </main>
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({question}) => {
+  const [hidden, setHidden] = useState(true);
+
+  return <div className='question'>
+    <header>
+      <h4>{question.title}</h4>
+      <button className='btn' onClick={() => setHidden(!hidden)}>
+        {hidden ? <AiOutlinePlus /> : <AiOutlineMinus />}
+      </button>
+    </header>
+    {hidden ? '' : <p>{question.info}</p>}
+  </div>;
 };
 
 export default Question;

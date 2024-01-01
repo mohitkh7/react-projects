74b5ebdd9f96802523c689573b85ee8e9e56168c
Mohit Khandelwal <mohitkh7@gmail.com>
2024-01-01 15:35:20 +0530

    completes project 1

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const List = () => {
+const List = ({people}) => {
   return (
     <>
-      <h2>list component</h2>
+    {people.map((person) => {
+      return (
+        <article className='person' key={person.id}>
+          <img className='img' src={person.image} alt={person.name}></img>
+          <div>
+            <h4>{person.name}</h4>
+            <p>{person.age} years</p>
+          </div>
+        </article>
+      );
+    })}
     </>
   );
 };

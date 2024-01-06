f4ec6563ec08690f5e5d7a14514d753d2a3a8c0c
Mohit Khandelwal <mohitkh7@gmail.com>
2024-01-07 00:02:57 +0530

    completes project 10

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,85 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({show: false, msg: '', type: ''});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      setAlert({show: true, msg: 'please enter value', type: 'danger'});
+    } else if (isEditing) {
+      setAlert({
+        show: true,
+        msg: 'Item edited successfully',
+        type: 'success'
+      });
+      setList(list.map((item) => {
+        if (item.id === editId) {
+          return {...item, title: name};
+        }
+        return item;
+      }));
+      setEditId(null);
+      setIsEditing(false);
+      setName('');
+    } else if (name) {
+      const newItem = {id: new Date().getTime().toString(), title: name};
+      setList([...list, newItem]);
+      setName('');
+      setAlert({
+        show: true,
+        msg: 'Item added successfully',
+        type: 'success'
+      });
+    }
+  }
+
+  const clearList = () => {
+    const alertObj = {show: true, msg:'List Cleared', type:'success'};
+    setAlert(alertObj);
+    setList([]);
+  }
+
+  const clearItem = (itemId) => {
+    setList(list.filter((item) => item.id !== itemId));
+    const alertObj = {show: true, msg:'Item Cleared', type:'success'};
+    setAlert(alertObj);
+  }
+
+  const activateEditMode = (itemId, currVal) => {
+    setEditId(itemId);
+    setIsEditing(true);
+    setName(currVal);
+  }
+
+  const removeAlert = () => {
+    const alertObj = {show: false, msg:'', type:''};
+    setAlert(alertObj);
+  }
+
+  return <section className='section-center'>
+    <form className='grocery-form' onSubmit={handleSubmit}>
+      {alert.show && <Alert {...alert} removeAlert={removeAlert} list={list}/>}
+      <h3>Grocery Bud</h3>
+      <div className='form-control'>
+        <input type='text' className='grocery' placeholder='e.g. Milk' 
+          value={name} onChange={(e) => {setName(e.target.value)}}/>
+        <button type='submit' className='submit-btn'>
+          {isEditing ? 'Edit' : 'Submit'}
+        </button>
+      </div>
+    </form>
+    {list.length > 0 && (
+      <div className='grocery-container'>
+        <List items={list} edit={activateEditMode} clear={clearItem}/>
+        <button className='clear-btn' onClick={clearList}>Clear Items</button>
+      </div>
+    )}        
+  </section>
 }
 
 export default App
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({items, edit, clear}) => {
+  return <div className='grocery-list'>
+    {items.map((item) => {
+      return <article key={item.id} className='grocery-item'>
+        <p className='title'>{item.title}</p>
+        <div className='btn-container'>
+          <button type='button' className='edit-btn' onClick={() => edit(item.id, item.title)}>
+            <FaEdit />
+          </button>
+          <button type='button' className='delete-btn' onClick={() => clear(item.id)}>
+            <FaTrash />
+          </button>
+        </div>
+      </article>
+    })}
+  </div>
 }
 
 export default List

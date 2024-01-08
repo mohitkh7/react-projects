38ba88946f9c6817ad99c6fea8394f3cf183ba7b
Mohit Khandelwal <mohitkh7@gmail.com>
2024-01-08 15:53:15 +0530

    add localstorage to project10

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -2,9 +2,17 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+const LS_KEY = 'list'
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem(LS_KEY);
+  return list ? JSON.parse(localStorage.getItem(LS_KEY)) : []
+}
+
+
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage);
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
   const [alert, setAlert] = useState({show: false, msg: '', type: ''});
@@ -62,6 +70,10 @@ function App() {
     const alertObj = {show: false, msg:'', type:''};
     setAlert(alertObj);
   }
+
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(list))
+  }, [list]);
 
   return <section className='section-center'>
     <form className='grocery-form' onSubmit={handleSubmit}>
